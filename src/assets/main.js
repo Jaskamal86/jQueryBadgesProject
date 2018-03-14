6c018746057a8c7d4a7f74e95fd23968f13e8c21
Jaskamal86 <37216600+Jaskamal86@users.noreply.github.com>
2018-03-14 12:15:41 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,33 @@ $(function() {
     url: 'https://www.codeschool.com/users/jaskamal86.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+      handleResponse(response.courses.completed);
     }
   });
 
+  function handleResponse(courses)
+  {
+    var $badges = $('#badges');
+    courses.forEach(function(course) {
+      
+      var course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+      
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($badges);
+      
+      $('<img />', {
+        src: course.badge
+      }).appendTo($badges);
+      
+       $('<a />', {
+        'class': 'btn btn-primary',
+         target: '_blank',
+         text: 'See Course'
+      }).appendTo($badges);
+      
+    })
+  }
 });
